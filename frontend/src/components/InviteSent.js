afd44f1e9474c50e3cdc2afe9d8dced7ec6a9c58
Davorin <ddoung@sfu.ca>
2018-04-15 01:56:15 -0700

    Show status properly for user invites

--- frontend/src/components/InviteSent.js
+++ frontend/src/components/InviteSent.js
@@ -4,12 +4,28 @@ import axios from 'axios';
 export default class InviteReceived extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			status: ''
+		}
 	}
 
+	showStatus() {
+		var status = ""
+		if(this.props.status==0){
+			status = "Pending"
+		}
+		else if(this.props.status==1){
+			status = "Accepted"
+		}
+		else{
+			status = "Declined"
+		}
+		return status
+	}
 	render() {
 		return (
 			<div>
-				Invite sent to <strong>{this.props.to_name}</strong> to join group <strong>{this.props.group_name}</strong> has status {this.props.status}
+				Invite sent to <strong>{this.props.to_name}</strong> to join group <strong>{this.props.group_name}</strong> has status <strong>{this.showStatus()}</strong>
 			</div>
 		);
 	}
